321548b04f2f4f79fcdc15ebe0ce6ef7579f93d4
jeffcky <2752154844@qq.com>
2023-10-14 00:37:19 +0800

    feat：添加支持迁移分支

--- test/KingbaseES.BasicTest/BlogContextFactory.cs
+++ test/KingbaseES.BasicTest/BlogContextFactory.cs
@@ -8,6 +8,7 @@ namespace KingbaseES.BasicTest
         public BlogContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<BlogContext>();
+
             optionsBuilder.UseKdbndp("host=localhost;port=54321;database=test;username=system;password=123456;");
 
             return new BlogContext(optionsBuilder.Options);

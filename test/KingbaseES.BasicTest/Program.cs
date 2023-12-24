3509de606f09ee103de0fe0c49bb7b4f57cab63f
jeffcky <2752154844@qq.com>
2023-12-24 23:48:15 +0800

    feat：修改基本测试命名空间

--- test/KingbaseES.BasicTest/Program.cs
+++ test/KingbaseES.BasicTest/Program.cs
@@ -1,53 +1,49 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿namespace KingbaseES.BasicTest;
 
-namespace KingbaseES.BasicTest
+internal static class Program
 {
-    internal static class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var services = new ServiceCollection();
+
+        services.AddDbContext<BlogContext>(options =>
         {
-            var services = new ServiceCollection();
+            options.UseKdbndp(@"host=localhost;port=54321;database=test;user id=system;password=123456;");   
+        });
 
-            services.AddDbContext<BlogContext>(options =>
-            {
-                options.UseKdbndp(@"host=localhost;port=54321;database=test;user id=system;password=123456;");   
-            });
+        var serviceProvider = services.BuildServiceProvider();
 
-            var serviceProvider = services.BuildServiceProvider();
+        var context = serviceProvider.GetRequiredService<BlogContext>();
 
-            var context = serviceProvider.GetRequiredService<BlogContext>();
+        context.Database.EnsureCreated();
 
-            context.Database.EnsureCreated();
-
-            // get list
-            var blogs = context.Blogs.ToList();
-            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "当前 Blogs 表中条目数：" + blogs.Count);
-            if (blogs.Any())
-            {
-                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "当前 Blogs 表中条目数大于1，进行清理...");
-                context.Blogs.RemoveRange(blogs);
-                context.SaveChanges();
-                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "清理完成！");
-                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "当前 Blogs 表中条目数：" + blogs.Count);
-            }
-            //add
-            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "新增 Blog 实体 值为I love EFCore!");
-            context.Add(new Blog() { Name = "I love EFCore!" });
-            var result = context.SaveChanges();
-
-            //update
-            var first = context.Blogs.FirstOrDefault();
-            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "查询结果:" + first.Name);
-
-            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "变更 Blog 实体 值为I love EFCore too!");
-            first.Name = "I love EFCore too!";
+        // get list
+        var blogs = context.Blogs.ToList();
+        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "当前 Blogs 表中条目数：" + blogs.Count);
+        if (blogs.Any())
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "当前 Blogs 表中条目数大于1，进行清理...");
+            context.Blogs.RemoveRange(blogs);
             context.SaveChanges();
-
-            // get
-            var testselect = context.Blogs.FirstOrDefault();
-            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "查询结果:" + testselect.Name);
-            Console.ReadKey();
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "清理完成！");
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "当前 Blogs 表中条目数：" + blogs.Count);
         }
+        //add
+        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "新增 Blog 实体 值为I love EFCore!");
+        context.Add(new Blog() { Name = "I love EFCore!" });
+        var result = context.SaveChanges();
+
+        //update
+        var first = context.Blogs.FirstOrDefault();
+        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "查询结果:" + first.Name);
+
+        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "变更 Blog 实体 值为I love EFCore too!");
+        first.Name = "I love EFCore too!";
+        context.SaveChanges();
+
+        // get
+        var testselect = context.Blogs.FirstOrDefault();
+        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "查询结果:" + testselect.Name);
+        Console.ReadKey();
     }
 }
